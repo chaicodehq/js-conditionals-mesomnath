4ff54e1dfe81e5e8e71287823b9051da5fea6859
mesomnath <somnathsahu.dev@gmail.com>
2026-02-10 12:48:46 +0530

    coffee-shop

--- src/07-coffee-shop.js
+++ src/07-coffee-shop.js
@@ -32,4 +32,42 @@
  */
 export function calculateCoffeePrice(size, type, extras = {}) {
   // Your code here
+  let price = 0;
+  switch(size.toLowerCase()){
+    case "small":
+      price += 3.00;
+      break;
+    case "medium":
+      price +=4.00;
+      break;
+    case "large":
+      price += 5.00;
+      break;
+    default:
+      return -1;
+  }
+  switch(type.toLowerCase()){
+    case "regular":
+      price += 0.00;
+      break;
+    case "latte":
+      price +=1.00;
+      break;
+    case "cappuccino":
+      price +=1.50;
+      break;
+    case "mocha":
+      price += 2.00;
+      break;
+    default:
+      return -1;
+  }
+  if(extras.extraShot)
+    price += 0.75;
+  if(extras.whippedCream)
+    price +=0.50;
+
+  return parseFloat(price.toFixed(2));
+  
+
 }
